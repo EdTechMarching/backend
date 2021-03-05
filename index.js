1227890eea0ffd54a9e2d2466380e07195378357
nickosp <nickospapageorge85@gmail.com>
2021-03-05 01:42:52 -0500

    set up index route

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 // Start route controllers
 const Planet = require('./models/planet-model.js');
+app.get('/planets', (req, res) => {
+    Planet.find({})
+    .then((planets) => {
+        res.render('planets/index', { todos });
+    })
+    .catch(console.error);
+})
 // end route controllers
 
 const port = process.env.PORT || 4000;
