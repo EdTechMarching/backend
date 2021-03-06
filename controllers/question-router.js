479dde6d7e008dbaaa7c3f4be69a758891d50908
ajose-malik <ajose.malik@gmail.com>
2021-03-06 11:51:21 -0500

    Created seed route

--- controllers/question-router.js
+++ controllers/question-router.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const seeder = require("../seeds/seeder")
 const questionRouter = express.Router()
 const Question = require("../models/question-model.js")
 
-// question router
+// routes
 questionRouter.get("/", (req, res) => {
 	Question.find({}, (err, found) => {
 		res.json(found)
@@ -11,9 +12,14 @@ questionRouter.get("/", (req, res) => {
 
 questionRouter.post("/", (req, res) => {
 	Question.create(req.body, (err, created) => {
-		Question.find({}, (err, found) => {
-			res.json(found)
-		})
+		res.json(created)
+	})
+})
+
+// seed routes
+questionRouter.get("/seed", (req, res) => {
+	Question.create(seeder, (err, created) => {
+		res.json(created)
 	})
 })
 module.exports = questionRouter
---
+++ seeds/seeder.js
@@ -0,0 +1,217 @@
+module.exports = [
+	{
+		answer: [
+			"Mars",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614967091/mars-25461_640_3_l5cutb.png"
+		],
+		selection: ["Mars", "Saturn", "Venus"],
+
+		hint: [
+			"I am an inner rocky planet and have water flowing on me.",
+			"I am the second smallest planet and about half of the size of Earth.",
+			"People named me after a Roman god of war because of my color, but my color is simply defined by the rusting process of the surface minerals."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614967030/mars-4628918_640_2_1_kxaldk.jpg",
+
+		popupRight: [
+			"LOCATION: Fourth rock from the sun.",
+			"DISTANCE FROM THE SUN: 128,409,598 to 154,865,853 miles.",
+			"AVERAGE SURFACE TEMPERATURE: -81° F (-63° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO MARS: 7 months.",
+			"GRAVITY: If you weigh 100 pounds (45 kilograms) on Earth, you’d weigh 38 pounds (17 kilograms) here."
+		],
+
+		popupBelow:
+			"Mars has been the subject of many myths. You intend to separate science fact from science fiction as you step on the surface of the red planet. For starters, the red planet isn’t even totally red! Browns, tans, golds, and flecks of green pop out as you scan the rocky, dusty, dune-laden landscape. Mars only looks red from far away because of rusting iron minerals in the rocks and soil. The soil blows into the air (occasionally in planet-wide dust storms) to give the atmosphere a bloody tint. Water may have flowed on Mars long ago, in ancient seas and riverbeds that early astronomers confused for canals. That was back when the atmosphere was thicker. Now, the air here (mostly carbon dioxide) is too wispy to support water or hold much heat. If you took off your spacesuit and stood on the equator at high noon, your toes would feel toasty but your face would be freezing! The good news is summer here lasts for six months. The bad news: So does winter!"
+	},
+	{
+		answer: [
+			"Jupiter",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989021/Jupiter_V_bhhicq.png"
+		],
+		selection: ["Venus", "Neptune", "Jupiter"],
+
+		hint: [
+			"I generate radiation levels more than a thousand times the lethal dose.",
+			"I may be the largest planet in the solar system, but I have the shortest day—just 10 hours—because of my rapid rotation.",
+			"I have nearly 70 moons."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614988453/Jupiter_1_nhuytr.png",
+
+		popupRight: [
+			"LOCATION: Fifth gas ball from the sun.",
+			"DISTANCE FROM THE SUN: 460,237,112 to 507,040,015 miles.",
+			"AVERAGE SURFACE TEMPERATURE: -234° F (-148° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO JUPITER: 13 months.",
+			"GRAVITY: If you weigh 100 pounds (45 kilograms) on Earth, you’d weigh 253 pounds (115 kilograms) here."
+		],
+
+		popupBelow:
+			"Jupiter is a gas giant, a ball of mostly hydrogen and helium large enough to hold more than 1,300 Earths. You won’t find any solid surface to explore here, and skydiving from your ship’s airlock would be a bad idea. You’d sink deeper into clouds of ammonia and water vapor until the intense atmospheric pressure and heat compressed the hydrogen around you into a molten liquid. Science fiction writers have proposed exploring Jupiter in hot-air balloons high above the crushing depths below, but you’re happy sipping cocoa aboard your ship in orbit. It’s a safer place to watch Jupiter’s spectacular cloud bands whiz by at more than 300 miles an hour (530 kilometers an hour)."
+	},
+	{
+		answer: [
+			"Earth",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989016/Earth_V_fr4wus.png"
+		],
+		selection: ["Mars", "Earth", "Uranus"],
+
+		hint: [
+			"I am one of the terrestrial planets, which means I have a central core, rocky mantle, and a crust that is solid.",
+			"Astronomers of ancient times thought that I was the center of the Universe.",
+			"My land masses move, a phenomenon known as “continental drift.” They creep about as fast as your fingernails grow."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614988915/Earth_1_lkese3.png",
+
+		popupRight: [
+			"LOCATION: Third rock from the sun.",
+			"DISTANCE FROM THE SUN: 91,402,640 to 94,509,460 miles.",
+			"AVERAGE SURFACE TEMPERATURE: 59° F (15° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH:  You're already here!"
+		],
+
+		popupBelow:
+			"Earth’s veil of atmosphere not only provides the right mix of breathable air for animals and plants, it also acts as a sort of force field against solar radiation and deadly space debris. As much as 22,000,000 pounds (10,000,000 kilograms) of meteors burn harmlessly in the Earth’s atmosphere each day. The solar system’s other planets are typically too hot or too cold to support liquid water, but Earth is just right. Life began more than 3.5 billion years ago in our oceans, which cover nearly 70 percent of the planet’s surface and are a source of the water vapor responsible for our weather. And before you get eager to blast off and explore the rest of the solar system, consider this: Scientists know more about the surface of the moon than they do the depths of the oceans. Our world still holds plenty of mysteries for the solar-system explorer."
+	},
+	{
+		answer: [
+			"Saturn",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989091/Saturn_V_gghx6l.png"
+		],
+		selection: ["Mercury", "Saturn", "Neptune"],
+
+		hint: [
+			"I am the only planet in the solar system that’s less dense than water.",
+			"I am an outer gas giant and have a diameter that is the second largest in the solar system.",
+			"More than 155,000 miles wide,my awe-inspiring ring system is the most complex of all the planets."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614988389/Saturn_1_zddqfh.png",
+
+		popupRight: [
+			"LOCATION: Sixth gas ball from the sun.",
+			"DISTANCE FROM THE SUN: 838,741,509 to 934,237,322 miles. ",
+			"AVERAGE TEMPERATURE: -288° F (-178° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO SATURN: 3 years.",
+			"GRAVITY: If you weigh 100 pounds (45 kilograms) on Earth, you’d weigh 107 pounds (49 kilograms) here."
+		],
+
+		popupBelow:
+			"You've probably used the word “awesome” a hundred times, but you never knew the word’s true meaning until you pull into the shade of Saturn’s rings. What appears as a solid disc from a distance is actually a glittering shower of ice and rock. And although the rings stretch almost as far into space as the distance between the Earth and the moon, they’re incredibly thin—typically about 30 feet (10 meters) wide. Astronomers believe Saturn’s rings formed from bits of asteroids and comets that shattered before they reached the planet. Swoop low over Saturn’s atmosphere and you might watch two of the planet’s moons rise within gaps in the rings. You couldn’t watch this moonrise from the planet’s surface, though. Like Jupiter, Uranus, and Neptune, Saturn is a gas giant, a ball of mostly hydrogen and helium more than 750 times the size of Earth. Its yellow color comes from ammonia crystals in the upper atmosphere, which is home to electrical storms the size of the United States. Steer clear of those lightning strikes. They’re a thousand times more powerful than Earth’s. And, yes, they’re awesome."
+	},
+	{
+		answer: [
+			"Mercury",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989035/Mercury_V_gjfgnw.png"
+		],
+		selection: ["Mars", "Mercury", "Neptune"],
+
+		hint: [
+			"I don’t have any moons, but my surface resembles that of the moon.",
+			"I am covered with a lot of craters that were caused by collisions with comets and meteoroids.",
+			"I am not only the planet that is the smallest in our solar system, I am also the planet that is closest to the sun."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614967030/mars-4628918_640_2_1_kxaldk.jpg",
+
+		popupRight: [
+			"LOCATION: First rock from the sun.",
+			"DISTANCE FROM THE SUN: 28,583,702 to 35,983,125 miles.",
+			"AVERAGE SURFACE TEMPERATURE: 243° F (117° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO MERCURY: 4 years.",
+			"GRAVITY: If you weigh 100 pounds on Earth, you’d weigh 38 pounds here."
+		],
+
+		popupBelow:
+			"Mercury is a planet of extremes. By day (which actually lasts roughly 30 Earth days), it’s one of the solar system’s most scorching spots—more than four times hotter than boiling water. By night, temperatures plummet hundreds of degrees below freezing. (The planet’s weak gravity can’t keep a grip on a heat-trapping atmosphere.) But despite its harsh environment, this hot-and-cold rock offers perks for the spacefarer. A weak magnetic field protects visitors from the sun’s deadly solar radiation (Earth has a similar but much stronger field), and the crater-pocked surface may contain valuable minerals. The speediest of all the planets, Mercury orbits the sun every 88 days—which means any Earthlings born here could celebrate four birthdays for your every one."
+	},
+	{
+		answer: [
+			"Uranus",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989095/Uranus_V_di04ot.png"
+		],
+		selection: ["Jupiter", "Neptune", "Uranus"],
+
+		hint: [
+			"I am one of two planets that spin in the opposite direction.",
+			"I have a huge diameter which makes me the third largest planet and the fourth in mass.",
+			"My temperatures and pressures are too extreme for scientists to consider that life could exist on me."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614986329/Uranus_1_yqjtje.png",
+
+		popupRight: [
+			"LOCATION: Seventh gas ball from the sun.",
+			"DISTANCE FROM THE SUN: 1,699,449,110 to 1,868,039,489 miles.",
+			"AVERAGE TEMPERATURE: -357° F (-216° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO URANUS: 9 years.",
+			"GRAVITY: If you weigh 100 pounds (45 kilograms) on Earth, you’d weigh 91 pounds (41 kilograms) here."
+		],
+
+		popupBelow:
+			"Like Jupiter and Saturn, Uranus is a gas giant—a ball of gas surrounding an Earth-size core of hot liquids. More specifically, Uranus is considered an “ice giant” because its atmosphere is composed mostly of “icy” water, ammonia, and methane. Researchers have found that Uranus’ crushing atmosphere can compress methane into precious rocks. Those methane clouds drifting far below your ship might be raining diamonds.Scientists suspect that a planet-size object knocked Uranus sideways in the early days of its formation. It has spun like a top toppling over ever since, which makes for some oddball seasons and decades devoid of even a glint of the sun’s faint light. The north pole is locked in more than 20 years of darkness in the winter and just as much sunlight in the summer, yet the temperature varies little here on the solar system’s coldest planet. Crank up the ship’s heater, young astronaut. You have wandered far from the sun."
+	},
+	{
+		answer: [
+			"Venus",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989098/Venus_V_fg5j4x.png"
+		],
+		selection: ["Venus", "Mercury", "Saturn"],
+
+		hint: [
+			"I am the only planet named after a female and it’s thought that it was because I am the brightest planet.",
+			"My average temperature is more than six times hotter than the hottest spot on Earth.",
+			"I have more volcanoes (more than 1,600) than any other planet in the solar system."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614986299/Venus_2_txdnvq.png",
+
+		popupRight: [
+			"LOCATION: Second rock from the sun.",
+			"DISTANCE FROM THE SUN: 66,782,596 to 67,693,905 miles.",
+			"AVERAGE SURFACE TEMPERATURE: 867° F (462° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO VENUS: 6 months.",
+			"GRAVITY: If you weigh 100 pounds on Earth, you’d weigh 91 pounds here."
+		],
+
+		popupBelow:
+			"Get the facts on Venus, sometimes called Earth's twin.You expected to find seas, mountains—maybe even a few trees—on a planet considered “Earth’s twin.” Instead, Venus is like Earth in reverse. A day here lasts longer than a year, the sun rises in the west and sets in the east, and—whew!—is it ever hot outside! Venus’ average temperature is more than six times hotter than the hottest spot on Earth. That’s hot enough to turn a slab of lead into a molten puddle. Sunset won’t bring relief from the scorching heat. Day or night, from its north pole to its south pole, every day of the year, Venus is locked in a perpetual heat wave. Blame the blanketing atmosphere, more than 90 times thicker than Earth’s, of carbon dioxide. Good thing your spacesuit has air conditioning! How did such a harsh planet earn the title of Earth’s twin? Venus is roughly the same size and density as Earth, which means the gravity is similar between here and home. Like Earth, Venus has clouds and wind—although the clouds are made of sulfuric acid and the wind exceeds tornado strength. The sunlight filtering from above bathes the landscape in a dim red glow. Venus is the brightest object in Earth’s sky after the moon."
+	},
+	{
+		answer: [
+			"Neptune",
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614989086/Neptune_V_nt4xxf.png"
+		],
+		selection: ["Mars", "Pluto", "Neptune"],
+
+		hint: [
+			"I wasn’t located by just using sight, but instead became the first planet found using mathematical calculations.",
+			"My magnetic field is 27 times mightier than Earth’s.",
+			"I am known for my beautiful blue color, which was used to name me after the Roman god of the sea."
+		],
+
+		popupImage:
+			"https://res.cloudinary.com/tinamarieg/image/upload/v1614988406/Neptune_1_bx0l7l.png",
+
+		popupRight: [
+			"LOCATION: Last gas ball from the sun.",
+			"DISTANCE FROM THE SUN: 2,771,162,074 to 2,819,185,846 miles.",
+			"AVERAGE TEMPERATURE: -353° F (-214° C).",
+			"LENGTH OF SPACE JOURNEY FROM EARTH TO NEPTUNE: 12 years.",
+			"GRAVITY: If you weigh 100 pounds (45 kilograms) on Earth, you’d weigh 114 pounds (52 kilograms) here."
+		],
+
+		popupBelow:
+			"You won’t need sunglasses for this voyage. High noon on Neptune is no brighter than the last few moments of sunset on Earth. Not that you could stand on Neptune at noon. Like Jupiter and Saturn, Neptune is a gas giant—a big ball of gas surrounding an Earth-size core of hot liquids rather than rocks or other solid matter. And like fellow “ice giant” Uranus, Neptune’s atmosphere is composed mostly of water, ammonia, and methane. It’s the methane that gives Neptune its striking blue hue. You’ll want to keep your ship soaring high in the planet’s atmosphere, which extends down to crushing depths and is home to the windiest weather in the solar system. Clouds of frozen methane whoosh as fast as a fighter jet through storms the size of Earth. Please wear your seatbelt for the duration of your flight above Neptune."
+	}
+]
